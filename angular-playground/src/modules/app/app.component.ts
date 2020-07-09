19846f8e1044e3f12b7c337b9f0432e1a35975da
Karan Rawal <kr14nov@outlook.com>
2020-07-09 12:28:34 +0530

    Simplified project structure.

--- angular-playground/src/modules/app/app.component.ts
+++ angular-playground/src/modules/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-playground';
+  title = 'Angular Playground';
 }
